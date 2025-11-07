dd90a962ccd69246af740f86b49c7b845071eb0e
AUstine <austineakhonya624@gmail.com>
2025-11-07 15:44:35 +0300

    first commit

---
+++ my-keja/my-website/src/App.jsx
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <main style={{ textAlign: "center" }}>
+        <h1>Home Page</h1>
+      </main>
+      
+    </>
+  );
+}
+export default App;
+
+
---
+++ my-keja/my-website/src/components/navbar.jsx
@@ -0,0 +1,27 @@
+function Navbar() {
+  return (
+  <nav className="bg-white text-black px-6 py-4 flex items-center justify-between">
+  {/* LEFT */}
+  <div className="text-xl font-galada">
+    Keja
+  </div>
+
+  {/* CENTER */}
+  <ul className="hidden md:flex gap-6 text-base">
+    <li><a href="#" className="hover:text-blue-600">Home</a></li>
+    <li><a href="#" className="hover:text-blue-600">About</a></li>
+    <li><a href="#" className="hover:text-blue-600">Contact</a></li>
+  </ul>
+
+  {/* RIGHT */}
+  <div>
+    <button className="bg-blue-600 text-white px-4 py-2 rounded-md hover:bg-blue-700">
+      Login
+    </button>
+  </div>
+</nav>
+
+  );
+}
+
+export default Navbar;
